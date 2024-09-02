dcfb2e2f871040089cc3d7dde6b772576901cc51
Caleb John <johncaleb022@gmail.com>
2024-09-02 10:17:20 -0700

    Update About.jsx

--- sections/About.jsx
+++ sections/About.jsx
@@ -16,7 +16,7 @@ const About = () => (
       viewport={{ once: false, amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
     >
-      <TypingText title="| About Metaversus" textStyles="text-center" />
+      <TypingText title="| About Metaspacex" textStyles="text-center" />
 
       <motion.p
         variants={fadeIn('up', 'tween', 0.2, 1)}
